88a1331263d05a203cb9137133585d0f5281f0cb
Boitumelo_Iven Moshitwa <itumelo553@gmail.com>
2023-05-13 15:55:42 +0200

    add date method,  post request method, a views folder that contains list ejs with form

--- app.js
+++ app.js
@@ -1,20 +1,29 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+let items = ['1st item']
 
 const app = express()
+app.set('view engine', 'ejs')
 app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
     const today = new Date()
-    const currentDay = today.getDate()
-
-    if (currentDay === 6 || currentDay === 0) {
-        res.send('its a weekend')
-    } else {
-        res.send('its a week day')
+    let options = {
+        weekday: 'long',
+        day: 'numeric',
+        month: 'long'
     }
+
+    let day = today.toLocaleDateString('en-US', options)
+    res.render('list', { kindOfDay: day, newListItems: items })
 })
 
+app.post('/', (req, res) => {
+    var item = req.body.newItem
+    items.push(item)
+    res.redirect('/')
+    console.log('your input is: ' + item)
+})
 
 app.listen(3000, () => {
     console.log('Server running at port 3000')
